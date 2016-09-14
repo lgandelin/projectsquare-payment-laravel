c2b20e7537c7d8470c839641ceee113c35da5d10
Louis Gandelin <lgandelin@web-access.fr>
2016-09-14 11:55:53 +0200

    Added users number slider in signup page

--- src/resources/lang/fr/signup.php
+++ src/resources/lang/fr/signup.php
@@ -6,6 +6,7 @@ return [
     'placeholder_agency_name' => 'ex: Mon Agence',
     'url' => 'URL souhaitée',
     'placeholder_url' => 'ex: mon-agence',
+    'check_url_disponibility' => 'Vérifier la disponibilité',
     'url_notice' => 'Vous pouvez définir l\'URL que vous souhaitez dans le champ ci-dessous, généralement il représentera le nom de votre agence web.',
     'url_notice_2' => 'Caractères autorisés : [a..z] [0..9] _ . -',
     'users_count' => 'Nombre d\'utilisateurs',
--- src/resources/views/signup/index.blade.php
+++ src/resources/views/signup/index.blade.php
@@ -4,13 +4,14 @@
     <meta charset="utf-8">
     <title>Inscription Projectsquare</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-slider/9.2.0/css/bootstrap-slider.min.css">
+    <script src="https://code.jquery.com/jquery-3.1.0.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-slider/9.2.0/bootstrap-slider.min.js"></script>
 </head>
 <body>
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                Logo Projectsquare
-
                 <h1 style="margin-bottom: 3rem;">{{ trans('projectsquare-payment::signup.title') }}</h1>
             </div>
         </div>
@@ -35,13 +36,17 @@
                 
                 <input class="form-control" type="text" placeholder="{{ trans('projectsquare-payment::signup.placeholder_url') }}" name="url" style="display: inline-block; width: 400px"/>
                 .projectsquare.io
+                <input type="button" class="btn btn-sm btn-primary" value="{{ trans('projectsquare-payment::signup.check_url_disponibility') }}" style="margin-top: 1rem;" />
             </div>
         </div>
 
         <div class="row">
             <div class="form-group col-lg-6 col-md-6" style="margin-bottom: 4rem">
-                <label for="url">{{ trans('projectsquare-payment::signup.users_count') }}</label><br>
-                <input class="form-control" type="number" placeholder="{{ trans('projectsquare-payment::signup.placeholder_users_count') }}" name="users_count" value="1" style="width: 75px"/>
+                <label for="url">{{ trans('projectsquare-payment::signup.users_count') }}</label>
+                <div style="margin-top: 2.5rem; margin-left: 1rem; cursor: pointer;">
+                    <input id="slider_users_count" data-slider-id='slider_users_count' type="text" data-slider-min="1" data-slider-max="30" data-slider-step="1" data-slider-value="1"/>
+                    <input class="form-control" type="number" name="users_count" value="1" style="display: inline-block; width: 50px; margin-left: 5rem; margin-right: 0.5rem; padding-right: 0; text-align: center" readonly /> utilisateur(s)
+                </div>
             </div>
         </div>
 
@@ -51,5 +56,17 @@
             </div>
         </div>
     </div>
+
+    <script>
+        $(document).ready(function() {
+            var slider = $("#slider_users_count").slider({
+                tooltip: 'always'
+            });
+
+            $("#slider_users_count").on("slide", function(slideEvt) {
+                $('input[name="users_count"]').val(slideEvt.value);
+            });
+        });
+    </script>
 </body>
 </html>
