981a50f02af9cf264035d785039bc5a290ce4670
Louis Gandelin <lgandelin@web-access.fr>
2016-12-06 11:40:12 +0100

    Added Google Analytics tracker

--- src/resources/views/includes/footer.blade.php
+++ src/resources/views/includes/footer.blade.php
@@ -1,5 +1,7 @@
     <div class="footer container">
         @include('projectsquare-payment::partials.footer')
     </div>
+
+    @include('projectsquare-payment::includes.ga_tracker')
 </body>
 </html>
