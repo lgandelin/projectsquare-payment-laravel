c7ab192acc367e74eac5f208eae54b894c091113
Louis Gandelin <lgandelin@web-access.fr>
2017-01-04 17:33:08 +0100

    Reduced font-size in features list in signup page

--- src/resources/views/landing_free_trial/index.blade.php
+++ src/resources/views/landing_free_trial/index.blade.php
@@ -8,40 +8,41 @@
     <script src="https://code.jquery.com/jquery-3.1.0.min.js"></script>
 </head>
 <body>
+    <div class="landing-free-trial-template container">
 
-<div class="landing-free-trial-template container">
+        <div class="signup">
+            @include('projectsquare-payment::signup.includes.header')
 
-    <div class="signup">
-        @include('projectsquare-payment::signup.includes.header')
+            @include('projectsquare-payment::signup.includes.simplify')
 
-        @include('projectsquare-payment::signup.includes.simplify')
+            @include('projectsquare-payment::signup.includes.features')
 
-        @include('projectsquare-payment::signup.includes.features')
+            <!-- FOOTER -->
+            <div class="footer">
+                <div class="row">
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <div class="blue-wrapper">
+                            <a class="start-free-trial button" href="#essai-gratuit">Démarrer mon essai gratuit</a>
+                        </div>
+                    </div>
+                </div>
 
-        <!-- FOOTER -->
-        <div class="footer">
-            <div class="row">
-                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                    <div class="blue-wrapper">
-                        <a class="start-free-trial button" href="#essai-gratuit">Démarrer mon essai gratuit</a>
+                <div class="row">
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <div class="green-wrapper">
+                            <span class="copyright">&copy; 2016</span>
+                        </div>
                     </div>
                 </div>
             </div>
-
-            <div class="row">
-                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                    <div class="green-wrapper">
-                        <span class="copyright">&copy; 2016</span>
-                    </div>
-                </div>
-            </div>
+            <!-- FOOTER -->
         </div>
-        <!-- FOOTER -->
+
+        <script>var route_free_trial_handler = "{{ route('landing_free_trial_handler') }}";</script>
+        <script src="{{ asset('js/signup.js') }}"></script>
+
+        @include('projectsquare-payment::includes.ga_tracker')
     </div>
 
-    <script>var route_free_trial_handler = "{{ route('landing_free_trial_handler') }}";</script>
-    <script src="{{ asset('js/signup.js') }}"></script>
-
-    @include('projectsquare-payment::includes.ga_tracker')
 </body>
 </html>
